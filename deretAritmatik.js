2dbb1e9ad842e2592cb0aa5597f6bc21633d3c33
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-03 17:25:56 +0700

    :rocket:

--- deretAritmatik.js
+++ deretAritmatik.js
@@ -1,10 +1,19 @@
 function tentukanDeretAritmatika(arr) {
-  arr;
+  var isAritmatik = true;
+  var difference = Math.abs(arr[0] - arr[1]);
+  difference;
+
+  for (var i = 0; i < arr.length - 1 && isAritmatik; i++) {
+    if (difference !== Math.abs(arr[i] - arr[i + 1])) {
+      isAritmatik = false;
+    }
+  }
+  return isAritmatik;
 }
 
 // TEST CASES
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
-// console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
-// console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
-// console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
-// console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
