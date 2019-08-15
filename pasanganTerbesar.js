4eee6d342f228df98ed24e89469f890d6cb926bc
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-15 13:52:24 +0700

    :rocket:

--- pasanganTerbesar.js
+++ pasanganTerbesar.js
@@ -1,21 +1,29 @@
 function pasanganTerbesar(num) {
-  var arrayNum = [];
-  var stringNum = String(num);
-  for (var i = 0; i < stringNum.length; i++) {
-    arrayNum.push([stringNum[i], stringNum[i + 1]]);
+  const arrNum = []
+  const stringNum = String(num)
+  const splitNum = []
+  let largestNum = 0
+
+  for (let i = 0; i < stringNum.length - 1; i++) {
+    let temp = ''
+    temp += stringNum[i] + stringNum[i + 1]
+    splitNum.push(temp)
   }
-  var map = arrayNum.map(function(num) {
-    return num.join('');
-  });
-  map.sort(function(a, b) {
-    return b - a;
-  });
-  return Number(map[0]);
+
+  for (let i = 0; i < splitNum.length; i++) {
+    arrNum.push(Number(splitNum[i]))
+  }
+
+  for (let i = 0; i < arrNum.length; i++) {
+    if (arrNum[i] > largestNum) largestNum = arrNum[i]
+  }
+
+  return largestNum
 }
 
 // TEST CASES
-console.log(pasanganTerbesar(641573)); // 73
-console.log(pasanganTerbesar(12783456)); // 83
-console.log(pasanganTerbesar(910233)); // 91
-console.log(pasanganTerbesar(71856421)); // 85
-console.log(pasanganTerbesar(79918293)); // 99
+console.log(pasanganTerbesar(641573)) // 73
+console.log(pasanganTerbesar(12783456)) // 83
+console.log(pasanganTerbesar(910233)) // 91
+console.log(pasanganTerbesar(71856421)) // 85
+console.log(pasanganTerbesar(79918293)) // 99
